72c9bb667b8d6917ed2e94b27b0e3267e463c0d5
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-08-01 13:25:34 +0900

    add opensl_stream

--- app/src/main/java/com/github/tommykw/audio/opensl_io.h
+++ app/src/main/java/com/github/tommykw/audio/opensl_io.h
@@ -6,5 +6,33 @@
 #define AUDIO_OPENSL_IO_H
 
 #include <SLES/OpenSLES.h>
+#include <SLES/OpenSLES_Android.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+typedef struct threadLock_{
+  pthread_mutex_t m;
+  pthread_cond_t c;
+  unsigned char s;
+} threadLock;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct opensl_stream {
+  SLObjectItf engineObject;
+  SLEngineItf engineEngine;
+
+  SLObjectItf outputMixObject;
+  SLObjectItf bgPlayerObject;
+  SLPlayItf bgPlayerPlay;
+  SLAndroidSimpleBufferQueueItf bgPlayerBufferQueue;
+  SLEffetSendItf bgPlayerEffectSend;
+
+  SLObjectItf recorderObject;
+  SLRecordItf recorderRecord;
+  SLAndroidSimpleBufferQueueItf recorderBufferQueue;
+}
 
 #endif //AUDIO_OPENSL_IO_H
