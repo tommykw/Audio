3fd58a368f1440254ddfbd1ad85aed6677e2eb5d
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-08-07 17:18:29 +0900

    add openSLPlayOpen

--- app/src/main/java/com/github/tommykw/audio/opensl_io.c
+++ app/src/main/java/com/github/tommykw/audio/opensl_io.c
@@ -29,4 +29,24 @@ static SLresult openSLCreateEngine(OPENSL_STREAM *p) {
 }
 
 static SLresult openSLPlayOpen(OPENSL_STREAM *p) {
+  SLresult result;
+  SLunit32 sr = p->sr;
+  SLunit32 channels = p->outchannels;
+
+  if (channels) {
+    SLDataLocator_AndroidSimpleBufferQueue loc_bufq =
+        {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE, 2}
+
+    switch (sr) {
+        case 8000:
+            sr = SL_SAMPLINGRATE_8;
+            break;
+        case 11025:
+            sr = SL_SAMPLINGRATE_11_025;
+            break;
+        case 16000:
+            sr = SL_SAMPLINGRATE_16;
+            break;
+    }
+  }
 }
