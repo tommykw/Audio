1f89b26eb6f851b52a684629fa3fb2fc3811f366
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-08-19 23:38:26 +0900

    add ex

---
+++ app/src/main/java/com/github/tommykw/audio/opensl_ex.c
@@ -0,0 +1,16 @@
+//
+// Created by tomitakenji on 2016/08/19.
+//
+
+#include <android/log.h>
+#include "opensl_io.h"
+
+#define BUFFERFRAMES 1024
+#define VECSAMPS_MONO 64
+#define VECSAMPS_STEREO 128
+#define SR 44100
+
+static int on;
+void start_process() {
+
+}
